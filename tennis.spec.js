eac9aba3c02325ff3e868c642ffe52f2ea356a6e
Pawanee Sangngern <Pawanee.san@gmail.com>
2017-09-09 01:21:44 +0700

    Add Tennis

--- tennis.spec.js
+++ tennis.spec.js
@@ -1,44 +1,182 @@
 const TennisGame = require('./tennispoint.js');
 
 
-
-
-
-
-
-
-
 //ไฟล์test
 test ('Echo "Love - Love" when game start',()=>{
-
-  //Arrange
-  let app = new TennisGame()
-
-
-
-  //Act
-  app.reset()
-  let result = app.echo()
-
-
-  //Assert
-  expect(result).toBe('Love - Love')
+          //Arrange
+          let app = new TennisGame()
+          //Act
+          app.reset()
+          let result = app.echo()
+          //Assert
+          expect(result).toBe('Love - Love')
 })
 
 
+
+
 test('Echo "Fifteen - Love" when playerA gets firt score',()=>{
+        //Arrange
+        let app = new TennisGame()
+        //Act
+        app.reset()
+        app.playerAgetScore()
+        let result = app.echo()
+        //Assert
+        expect(result).toBe('Fifteen - Love')
+})
 
-//Arrange
 
+
+test('Echo "Thirty - Fifteen" when playerA gets two score and playerB gets firt score',()=>{
+        //Arrange
+        let app = new TennisGame()
+        //Act
+        app.reset()
+        app.playerAgetScore()
+        app.playerAgetScore()
+        app.playerBgetScore()
+        let result = app.echo()
+        //Assert
+        expect(result).toBe('Thirty - Fifteen')
+})
+
+
+
+test('Echo "Thirty - Thirty" when playerA and playerB gets two score',()=>{
+      //Arrange
+      let app = new TennisGame()
+      //Act
+      app.reset()
+      app.playerAgetScore()
+      app.playerAgetScore()
+      app.playerBgetScore()
+      app.playerBgetScore()
+
+      let result = app.echo()
+      //Assert
+      expect(result).toBe('Thirty - Thirty')
+})
+
+
+
+
+test('Echo "The winner is player A"when playerA gets last score after game point',()=>{
 let app = new TennisGame()
-app.playerAgetScore()
 
 //Act
+app.reset()
+app.playerAgetScore()
+app.playerAgetScore()
+app.playerAgetScore()
+app.playerAgetScore()
+
+
 let result = app.echo()
-
-//Assert
-expect(result).toBe('Fifteen - Love')
+expect(result).toBe('The winner is player A')
 
 
+
+
+
+
+})
+
+
+
+
+
+
+
+test('Echo "The winner is player B"when playerB gets last score after game point',()=>{
+let app = new TennisGame()
+
+//Act
+app.reset()
+app.playerBgetScore()
+app.playerBgetScore()
+app.playerBgetScore()
+app.playerBgetScore()
+
+
+let result = app.echo()
+expect(result).toBe('The winner is player B')
+
+
+
+
+
+
+})
+
+
+test('Echo "deuce" when playerA and playerB points are always the same',()=>{
+  let app = new TennisGame()
+  app.reset()
+  app.playerAgetScore()
+  app.playerAgetScore()
+  app.playerAgetScore()
+  app.playerBgetScore()
+  app.playerBgetScore()
+  app.playerBgetScore()
+
+  let result = app.echo()
+  expect(result).toBe('deuce')
+
+
+})
+
+
+test('Echo "Advantage playerA" when playerA gets score after deuce',()=>{
+  let app = new TennisGame()
+  app.reset()
+  app.playerAgetScore()
+  app.playerAgetScore()
+  app.playerAgetScore()
+  app.playerBgetScore()
+  app.playerBgetScore()
+  app.playerBgetScore()
+  app.playerAgetScore()
+
+  let result = app.echo()
+  expect(result).toBe('Advantage playerA')
+
+})
+
+
+test('Echo "Advantage playerB" when playerB gets score after deuce',()=>{
+  let app = new TennisGame()
+  app.reset()
+  app.playerAgetScore()
+  app.playerAgetScore()
+  app.playerAgetScore()
+  app.playerBgetScore()
+  app.playerBgetScore()
+  app.playerBgetScore()
+  app.playerBgetScore()
+
+  let result = app.echo()
+  expect(result).toBe('Advantage playerB')
+
+})
+
+test('Echo "The winner is player B" when playerB gets score after advantage',()=>{
+  let app = new TennisGame()
+  app.reset()
+
+  app.playerAgetScore()
+  app.playerAgetScore()
+  app.playerAgetScore()
+  app.playerBgetScore()
+  app.playerBgetScore()
+  app.playerBgetScore()
+  app.playerBgetScore()
+  app.playerBgetScore()
+
+
+
+
+  let result = app.echo()
+  expect(result).toBe('The winner is player B')
 
 })
--- tennispoint.js
+++ tennispoint.js
@@ -1,18 +1,47 @@
 function TennisGame() {
     const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
     this.playerAScore = 0
+    this.playerBScore = 0
 
     this.reset = () => {
         this.playerAScore = 0
+        this.playerBScore = 0
     }
 
     this.echo = () => {
-       return `${scoreString[this.playerAScore]} - Love`
+
+      if((this.playerAScore > 3 && this.playerBScore != 3) || (this.playerAScore == 5 && this.playerBScore == 3)){
+        return "The winner is player A"
+      }else if((this.playerBScore > 3 && this.playerAScore != 3) || (this.playerBScore == 5 && this.playerAScore == 3)){
+        return "The winner is player B"
+
+      }else if (this.playerAScore == 3 && this.playerBScore == 3) {
+        return "deuce"
+
+      }else if(this.playerAScore > 3 && this.playerBScore == 3){
+        return "Advantage playerA"
+
+      }else if (this.playerBScore > 3 && this.playerAScore == 3) {
+        return "Advantage playerB"
+
+      }else{
+          return `${scoreString[this.playerAScore]} - ${scoreString[this.playerBScore]}`
+      }
+
+
+
     }
 
     this.playerAgetScore = () => {
-        this.playerAScore++
+      if(this.playerBScore > 3) this.playerBScore--
+      else this.playerAScore++
     }
+
+    this.playerBgetScore = () => {
+      if(this.playerAScore > 3) this.playerAScore--
+      else this.playerBScore++
+    }
+
 }
 
 module.exports = TennisGame
